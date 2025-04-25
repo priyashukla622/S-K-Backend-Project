5d116ae5cd3ea3665a5c45d495960e7661f709d2
priya shukla <priyashukla22@navgurukul.org>
2025-04-25 17:53:43 +0530

    add space

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-const port=5000;
 
+const port=5000;
 const app=express();
+
+
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
