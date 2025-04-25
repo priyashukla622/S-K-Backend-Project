20cf9d7107468827011c9e092bafddde8f420cae
priya shukla <priyashukla22@navgurukul.org>
2025-04-25 15:01:37 +0530

    added .env file

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const port=5000;
 const app=express();
+
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
